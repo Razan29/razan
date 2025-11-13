5d5bf90e1abc976f69551bbb79be7f8c94b7a487
Razan29 <razankhateb81@gmail.com>
2025-11-13 10:35:12 +0200

    game2

---
+++ Game2/app/src/main/java/com/example/game2/ScoreActivity.java
@@ -0,0 +1,34 @@
+package com.example.game2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoreActivity extends AppCompatActivity {
+    private Button back;
+    private TextView score, games;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_score);
+        Intent MainAC= new Intent(ScoreActivity.this,MainActivity.class);
+        games=findViewById(R.id.gamesi);
+        score=findViewById(R.id.scorei);
+        back=findViewById(R.id.backid);
+        games.setText("games="+MainActivity.countg);
+        score.setText("score="+MainActivity.countA);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(MainAC);
+            }
+        });
+        };
+    }
